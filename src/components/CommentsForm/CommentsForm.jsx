2b7fc0a5b1a0c5783bafeeffda9e5050863d3b9a
Tim Miner <poeminminer1@gmail.com>
2022-07-16 17:32:51 -0500

    added history to go to the next url on ibutton click

--- src/components/CommentsForm/CommentsForm.jsx
+++ src/components/CommentsForm/CommentsForm.jsx
@@ -1,12 +1,18 @@
+import { useHistory } from 'react-router-dom';
 
 function CommentsForm() {
+    const history = useHistory();
+
+    const handleNextClick = () => {
+        history.push('/review');
+    }
 
     return (
         <>
             <h2 className="question"></h2>
             <label>Any additional comments about today?</label>
             <input type="text" placeholder="Message Here" />
-            <input type="button" value="Next" />
+            <input type="button" value="Next" onClick={handleNextClick} />
         </>
     )
 }
--- src/components/SupportForm/SupportForm.jsx
+++ src/components/SupportForm/SupportForm.jsx
@@ -1,12 +1,18 @@
+import { useHistory } from 'react-router-dom';
 
 function SupportForm() {
+
+    const history = useHistory();
+    const handleNextClick = () => {
+        history.push('/comments');
+    }
 
     return (
         <>
             <h2 className="question"></h2>
             <label>Are you feeling supported today?</label>
             <input type="number" placeholder="Number 1-5" />
-            <input type="button" value="Next" />
+            <input type="button" value="Next"  onClick={handleNextClick}/>
         </>
     )
 }
